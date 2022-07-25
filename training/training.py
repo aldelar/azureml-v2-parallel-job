c8d375c1c0f7b22e4d577bb49664966d24018f37
Alexandre Delarue <aldelar@microsoft.com>
2022-07-25 11:10:43 -0700

    adding environment variable to training job

--- training/training.py
+++ training/training.py
@@ -4,15 +4,21 @@ from azureml_user.parallel_run import EntryScript
 # init() called once per process
 def init():
 
+    # retrieve parameters from the entry script
     parser = argparse.ArgumentParser()
     parser.add_argument("--param_1", type=str)
     args, unknown = parser.parse_known_args()
     global param_1
     param_1 = args.param_1
 
+    # retrieve environment variables
+    global env_var_1
+    env_var_1 = os.environ['env_var_1']
+
+    # AML logger
     global logger
     logger = EntryScript().logger
-    logger.info(f"train init(), param_1:{param_1}")
+    logger.info(f"train init(), param_1:{param_1}, env_var_1:{env_var_1}")
    
 # run() called as many times as needed to process all files in the parallel job input
 def run(mini_batch):
@@ -23,7 +29,7 @@ def run(mini_batch):
         tenant_basename = os.path.basename(tenant_file_path)
         with open(tenant_file_path,'r') as tenant_file:
             tenant_dict = json.load(tenant_file)
-        logger.info(f"train processing({tenant_basename} => {tenant_dict}) with param_1:{param_1}")
+        logger.info(f"train processing({tenant_basename} => {tenant_dict}) with param_1:{param_1}, env_var_1:{env_var_1}")
         time.sleep(0.5) # simulate some small processing
         results.append(f"{tenant_basename} processed")
 
