afb82d60507c623a351438ccd8f122f1af8cca5d
Alexandre Delarue <aldelar@microsoft.com>
2022-08-03 11:31:04 -0700

    adding output of training job collected to a specific datalake location

--- training/training.py
+++ training/training.py
@@ -7,9 +7,12 @@ def init():
     # retrieve parameters from the entry script
     parser = argparse.ArgumentParser()
     parser.add_argument("--param_1", type=str)
+    parser.add_argument("--prediction_data_folder", type=str)
     args, unknown = parser.parse_known_args()
     global param_1
+    global prediction_data_folder
     param_1 = args.param_1
+    prediction_data_folder = args.prediction_data_folder
 
     # retrieve environment variables
     global env_var_1
@@ -31,6 +34,10 @@ def run(mini_batch):
             tenant_dict = json.load(tenant_file)
         logger.info(f"train processing({tenant_basename} => {tenant_dict}) with param_1:{param_1}, env_var_1:{env_var_1}")
         time.sleep(2) # simulate some small processing
+        # simulate write predictions to the output folder
+        with open(os.path.join(prediction_data_folder,tenant_basename),'w') as prediction_file:
+            json.dump(tenant_dict,prediction_file)
+        # log to global results object
         results.append(f"{tenant_basename} processed")
 
     return results
